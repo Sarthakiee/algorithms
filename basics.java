7eae5f51287746211e4d95098e4b4be906668987
Sarthakk <sarthak90884@gmail.com>
2026-03-08 23:06:48 +0530

    Updated basics.java

--- basics.java
+++ basics.java
@@ -2,6 +2,19 @@ import java.util.Scanner;
 class basics 
 {
     public static void main(String[] args) {
+        // Area of a rectangle
+        /* 
+        DECLARE LENGTH : REAL
+        DECLARE WIDTH : REAL
+        DECLARE RECT  : REAL
+        DECLARE AREA : REAL 
+        
+        READ LENGTH
+        READ WIDTH
+        RECT = LENGTH * WIDTH
+        AREA = RECT
+        */
+        // code snippet for area of a rectangle
      Scanner sc = new Scanner(System.in);
         System.out.println("Enter the length of the rectangle:");
         int length = sc.nextInt();
