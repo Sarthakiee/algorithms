60e16b3d2aee5d604b803934f661129b1748708a
Sarthakk <sarthak90884@gmail.com>
2026-03-08 15:38:14 +0530

    Added Bitwise implementations

---
+++ bitwise.java
@@ -0,0 +1,14 @@
+public class bitwise {
+    public static void main(String[] args) {
+        int a ;
+        int b ;
+        int c ;
+
+        c = 8; 
+        b = 2;
+        a = c/b;
+        c = b >> a; 
+        c = b << a;
+        System.out.println(c);
+    }
+}
